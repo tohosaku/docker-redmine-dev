a5916c0b8f23f94382ec465b64cdccb9391403b3
tohosaku <tohosaku@users.osdn.me>
2024-01-18 18:36:37 +0900

    Update script

--- dc.sh
+++ dc.sh
@@ -2,4 +2,8 @@
 
 . ./user.sh
 
-LOCAL_UID=${LOCAL_UID} LOCAL_GID=${LOCAL_GID} docker compose $* 
+if [ "$#" -eq 0 ]; then
+  LOCAL_UID=${LOCAL_UID} LOCAL_GID=${LOCAL_GID} docker compose exec -it redmine tmux
+else
+  LOCAL_UID=${LOCAL_UID} LOCAL_GID=${LOCAL_GID} docker compose $*
+fi
